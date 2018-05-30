5e0e6f930658415164f03f097a15df281562078b
sunny <sunny@sunnydeMacBook-Pro.local>
2018-05-30 23:33:45 +0800

    init

---
+++ Example/Pods/Target Support Files/AutoLayoutHidden/AutoLayoutHidden-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AutoLayoutHidden : NSObject
+@end
+@implementation PodsDummy_AutoLayoutHidden
+@end
---
+++ Example/Pods/Target Support Files/Pods-AutoLayoutHidden_Tests/Pods-AutoLayoutHidden_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AutoLayoutHidden_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AutoLayoutHidden_Tests
+@end
